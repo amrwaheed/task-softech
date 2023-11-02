000df51f40293219676760c9020d1ace702864ac
amrwaheed <amrwaheed702@gmail.com>
2023-11-02 18:49:21 +0200

    feat: setup basic structure project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { NotFoundComponent } from '@softech/shared-component';
+ 
 const routes: Routes = [
+  {path:'', pathMatch:'full', redirectTo:'softech'},
+  //* here we use routes with header
+  {path:'softech' , loadChildren:()=> import('./main/main.module').then(m=>m.MainModule)},
+
+
+  {path:'**' , component: NotFoundComponent}
 
 ];
 
