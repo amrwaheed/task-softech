d614d2e3e4028b26212de88ba42de9f3e58a6606
amrwaheed <amrwaheed702@gmail.com>
2023-11-03 23:44:01 +0200

    feat: add test cases

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('task-tech');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('task-tech app is running!');
-  });
+ 
 });
